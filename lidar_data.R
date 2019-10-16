b1e2ca4c63441bac5e329e9e7cc9e062fc62dee0
richardjtelford <richard.telford@bio.uib.no>
2019-10-16 11:10:56 +0200

    initial commit

---
+++ lidar_data.R
@@ -0,0 +1,46 @@
+#import packages
+library("tidyverse")
+library("raster")
+library("dynatopmodel")
+
+#import dem
+dtm <- list.files(path = "data", 
+                  pattern = "DTM.*tif$", 
+                  recursive = TRUE, 
+                  full.names = TRUE) %>% 
+  map(raster) %>% 
+  do.call(what = merge)
+
+#plot elevation
+sp::plot(dtm, main=c("dtm"))
+
+#calculate slope and aspect
+slope_aspect <- terrain(dtm, opt=c("slope", "aspect"), unit = "degrees")
+
+sp::plot(slope_aspect, main = c("Slope", "Aspect"))
+
+#upslope area and topographic wetness index
+a.atb <- upslope.area(dtm, atb = TRUE)
+par(tcl = -0.2, mgp = c(2.5, .5, 0))
+sp::plot(a.atb, main=c("Upslope area (log(m^2/m))", "TWI log(m^2/m)"))
+
+
+#test
+data(brompton)
+
+tst <- upslope.area(brompton$dem, atb=TRUE)
+sp::plot(tst, main=c("Upslope area (log(m^2/m))", "TWI log(m^2/m)"))
+
+
+#dom
+dom <- list.files(path = "data", 
+                  pattern = "DOM.+\\.tif$", 
+                  recursive = TRUE, 
+                  full.names = TRUE) %>% 
+  map(raster) %>% 
+  do.call(what = merge)
+
+#veg height
+delta <- dom-dtm
+delta
+sp::plot(delta, main = "delta")
