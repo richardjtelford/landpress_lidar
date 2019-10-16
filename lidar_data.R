83f257fc705ff4c953d20a4cecbad329a6acd0d0
richardjtelford <richard.telford@bio.uib.no>
2019-10-16 12:25:04 +0200

    added test data

--- lidar_data.R
+++ lidar_data.R
@@ -3,6 +3,25 @@ library("tidyverse")
 library("raster")
 library("dynatopmodel")
 
+#single tile test
+dtm1 <- raster("data/NDH Nordhordland 5pkt 2018/data/dtm/DTM_32-1-466-153-62.tif")
+
+#plot elevation
+sp::plot(dtm1, main=c("dtm"))
+
+#calculate slope and aspect
+slope_aspect <- terrain(dtm, opt=c("slope", "aspect"), unit = "degrees")
+
+sp::plot(slope_aspect, main = c("Slope", "Aspect"))
+
+#upslope area and topographic wetness index
+a.atb1 <- upslope.area(dtm1, atb = TRUE)
+par(tcl = -0.2, mgp = c(2.5, .5, 0))
+sp::plot(a.atb1, main=c("Upslope area (log(m^2/m))", "TWI log(m^2/m)"))
+
+
+
+
 #import dem
 dtm <- list.files(path = "data", 
                   pattern = "DTM.*tif$", 
